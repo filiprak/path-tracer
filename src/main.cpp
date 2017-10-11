9ea06b46a1b8975e3ec28ab87cc38e186642aa76
Filip Rak <filiprak@wp.pl>
2017-10-11 11:54:13 +0200

    Add assimp v3.3.1 headers and dll loader lib.

--- src/main.cpp
+++ src/main.cpp
@@ -1,8 +1,23 @@
 #include "main.h"
 #include "view.h"
 
+#include <assimp/Importer.hpp>
+#include <assimp/scene.h>
+#include <assimp/postprocess.h>
+
 int main() {
 	printf("Starting path-tracer application.\n");
+
+	Assimp::Importer imp;
+
+	const aiScene* scene = imp.ReadFile("teapot.obj",
+		aiProcess_CalcTangentSpace |
+		aiProcess_Triangulate |
+		aiProcess_JoinIdenticalVertices |
+		aiProcess_SortByPType);
+	
+	if (scene)
+		printf("Loaded scene from file using assimp.");
 
 	viewInit(300, 300);
 
